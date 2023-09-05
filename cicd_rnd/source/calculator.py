33e26475c8ef346b81d80fd983c5e0df7d663b9c
ibasantjoshi786 <144104955+ibasantjoshi786@users.noreply.github.com>
2023-09-05 21:18:54 +0530

    Add files via upload
    initial commit

---
+++ cicd_rnd/source/calculator.py
@@ -0,0 +1,14 @@
+def addition(no1, no2):
+    return no1 + no2
+
+
+def subtraction(no1, no2):
+    return no1 - no2
+
+
+def multiplication(no1, no2):
+    return no1 * no2
+
+
+def divide(no1, no2):
+    return no1 / no2
---
+++ cicd_rnd/test_suite/test_calculator.py
@@ -0,0 +1,26 @@
+import pytest
+from cicd_rnd.source import calculator as calci
+
+
+@pytest.mark.parametrize('no1, no2, expected_result', [(8, 3, 11), (4, 2, 6)])
+def test_addition(no1, no2, expected_result):
+    result = calci.addition(no1, no2)
+    assert result == expected_result
+
+
+@pytest.mark.parametrize('no1, no2, expected_result', [(8, 3, 5), (4, 2, 2)])
+def test_subtraction(no1, no2, expected_result):
+    result = calci.subtraction(no1, no2)
+    assert result == expected_result
+
+
+@pytest.mark.parametrize('no1, no2, expected_result', [(8, 3, 24), (4, 2, 8)])
+def test_multiplication(no1, no2, expected_result):
+    result = calci.multiplication(no1, no2)
+    assert result == expected_result
+
+
+@pytest.mark.parametrize('no1, no2, expected_result', [(8, 4, 2), (6, 2, 3)])
+def test_divide(no1, no2, expected_result):
+    result = calci.divide(no1, no2)
+    assert result == expected_result
