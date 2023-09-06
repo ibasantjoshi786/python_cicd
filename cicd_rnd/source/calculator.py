64cb817110b84efe8c224f6694c1df7f77b5e94b
Basant Joshi <bjoshi@aceindia>
2023-09-06 10:15:11 +0530

    intendation done

--- cicd_rnd/source/calculator.py
+++ cicd_rnd/source/calculator.py
@@ -13,8 +13,12 @@ def multiplication(no1, no2):
 def divide(no1, no2):
     return no1 / no2
 
+
 def module(no1, no2):
     return no1 % no2
+
 	
 def percentage(no1, no2):
     return 100 * no1 / no2 
+	
+
