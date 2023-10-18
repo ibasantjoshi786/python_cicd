a84b2309ad825db942dda8a4e14625d6903ed604
ibasantjoshi786 <144104955+ibasantjoshi786@users.noreply.github.com>
2023-10-18 17:07:51 +0530

    app.py

---
+++ cicd_rnd/source/app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+# Create a Flask web application
+app = Flask(__name__)
+
+# Define a route for the root URL
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+if __name__ == '__main__':
+    # Run the Flask application on localhost and port 5000
+    app.run()
